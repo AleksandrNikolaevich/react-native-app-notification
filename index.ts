b21e8229af9c63b493ad37f83676491a57e8e1b7
aleksandr <aleksandr.nikolaevich.ch@ya.ru>
2018-09-02 16:17:37 +0300

    fix export AlertTypes

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import NotifiacationProvider from "./src/NotificationProvider";
 import withNotify, {WithNotify} from "./src/withNotify";
+import {AlertTypes} from "./src/AlertTypes";
 export {
     NotifiacationProvider,
     withNotify,
-    WithNotify
+    WithNotify,
+    AlertTypes
 }
